7c09f0f32cc4db43ee7fca4055854e9511220713
puppe1990 <matheus.puppe90@hotmail.com>
2025-09-21 12:49:12 -0300

    Add layout components for Contato and Politica de Privacidade pages
    - Implemented default layout components for the Contato and Politica de Privacidade pages, allowing for consistent rendering of child components.
    - This update enhances the structure and organization of the application, improving maintainability and readability of the codebase.

--- src/app/politica-de-privacidade/layout.tsx
+++ src/app/politica-de-privacidade/layout.tsx
@@ -1,3 +1,11 @@
 import { metadata } from './metadata'
 
 export { metadata }
+
+export default function PoliticaPrivacidadeLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
