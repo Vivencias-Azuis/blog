0e091064eca3d722f9cc935d5f400bac57b201fe
puppe1990 <matheus.puppe90@hotmail.com>
2025-12-22 11:13:07 -0300

    Update ad script loading strategy for improved performance
    - Changed the loading strategy of the Google Ads script from "afterInteractive" to "lazyOnload" to enhance page performance and reduce initial load time.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -115,7 +115,7 @@ export default function RootLayout({
         </div>
         <Script
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4134431815923665"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
           crossOrigin="anonymous"
         />
         <PWAStatus />
